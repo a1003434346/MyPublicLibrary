5a1eb30336923a01e19aa65074e91151e97f9dd5
lee <good14@126.com>
2019-12-18 15:09:20 +0800

    PageUtils 增加title状态改变 增加颜色过渡

--- mypubliclibrary/src/main/java/com/example/mypubliclibrary/util/PageUtils.java
+++ mypubliclibrary/src/main/java/com/example/mypubliclibrary/util/PageUtils.java
@@ -1,6 +1,7 @@
 package com.example.mypubliclibrary.util;
 
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * 功能:跟ViewPage有关的工具类
@@ -25,4 +26,44 @@ public class PageUtils {
             leaveView.setScaleY((1.0f + additionScale) + (-additionScale * positionOffset));
         }
     }
+
+    /**
+     * 设置标题颜色的过渡
+     *
+     * @param enterView      进入的标题View
+     * @param leaveView      离开的标题View
+     * @param enterColor     进入标题的颜色
+     * @param leaveColor     离开标题的颜色
+     * @param positionOffset Page的回调
+     */
+    public static void setTitleColorTran(View enterView, View leaveView, int enterColor, int leaveColor, float positionOffset) {
+        //离开页面标题的颜色
+        int leavePageColor = ColorUtils.blendColors(enterColor, leaveColor, positionOffset);
+        //进入页面标题的颜色
+        int enterPageColor = ColorUtils.blendColors(leaveColor, enterColor, positionOffset);
+        ((TextView) enterView).setTextColor(enterPageColor);
+        ((TextView) leaveView).setTextColor(leavePageColor);
+    }
+
+    /**
+     * title状态改变
+     *
+     * @param enterColor    进入的颜色
+     * @param leaveColor    离开的颜色
+     * @param additionScale 大于0代表有缩放
+     */
+    public static void titleChange(View enterView, View leaveView, int enterColor, int leaveColor, float additionScale) {
+        ((TextView) enterView).setTextColor(enterColor);
+        ((TextView) leaveView).setTextColor(leaveColor);
+        if (additionScale > 0) {
+            enterView.setScaleX(1.0f + additionScale);
+            enterView.setScaleY(1.0f + additionScale);
+            if (leaveView.getScaleX() > 1.0f && leaveView.getScaleY() > 1.0f) {
+                leaveView.setScaleX(1.0f);
+                leaveView.setScaleY(1.0f);
+            }
+        }
+    }
+
+
 }
