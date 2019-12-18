24a71ecb3c7610c5f0a94eef27be5968ea3baa6b
lee <good14@126.com>
2019-12-18 14:37:45 +0800

    ViewUtils增加ViewPage的标题缩放

--- mypubliclibrary/src/main/java/com/example/mypubliclibrary/util/ViewUtils.java
+++ mypubliclibrary/src/main/java/com/example/mypubliclibrary/util/ViewUtils.java
@@ -164,4 +164,24 @@ public class ViewUtils {
     public static int getViewIdCode(int id) {
         return Integer.parseInt((id + "").substring(6));
     }
+
+
+    /**
+     * 设置ViewPage的标题缩放
+     *
+     * @param enterView      进入的标题View
+     * @param leaveView      离开的标题View
+     * @param additionScale  在当前大小的基础上增加尺度
+     * @param positionOffset Page的回调
+     */
+    public static void setPageTitleScale(View enterView, View leaveView, float additionScale, float positionOffset) {
+        //设置进入的缩放
+        enterView.setScaleX(1.0f + additionScale * positionOffset);
+        enterView.setScaleY(1.0f + additionScale * positionOffset);
+        //设置离开的缩放
+        if (leaveView.getScaleX() > 1.0f) {
+            leaveView.setScaleX((1.0f + additionScale) + (-additionScale * positionOffset));
+            leaveView.setScaleY((1.0f + additionScale) + (-additionScale * positionOffset));
+        }
+    }
 }
